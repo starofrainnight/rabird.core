7361d63d41a6f89600ae831f9bbe875249e22a45
Hong-She Liang <starofrainnight@gmail.com>
2015-04-11 20:21:59 +0800

    Simplified source preprocess procedure
    Renamed preprocess_sources_for_compatible() to preprocess_sources()
    Removed logging informations during setup()
    Fixed preprocess source directory layout
    Simplified setup.py
    Update version to 0.1.7

--- setup.py
+++ setup.py
@@ -9,22 +9,18 @@ import sys
 import shutil
 import logging
 import fnmatch
-import src.rabird.core.logging as rabird_logging
 from src.rabird.core import __version__, distutils
 from setuptools import setup, find_packages
 
-from_package = 'src'
-to_package = 'rabird'
-package_name = 'rabird.core'
-
-rabird_logging.load_default_config()
+namespace = "rabird"
+package_name = '%s.%s' % (namespace, "core")
 
 # Convert source to v2.x if we are using python 2.x.
-distutils.preprocess_sources_for_compatible(from_package, os.path.realpath(os.curdir))
+source_dir = distutils.preprocess_source()
 
 # Exclude the original source package, only accept the preprocessed package!
-our_packages = find_packages(exclude=[from_package, '%s.*' % from_package])
-
+our_packages = find_packages(where=source_dir)
+    
 our_requires = [
     'six>=1.3.0'
     ]
@@ -42,8 +38,7 @@ setup(
     version=__version__,
     author='Hong-She Liang',
     author_email='starofrainnight@gmail.com',
-    url='',
-    py_modules=[to_package],
+    url='https://github.com/starofrainnight/%s' % package_name,
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
@@ -52,6 +47,7 @@ setup(
         'Topic :: Software Development :: Libraries',
     ],
     install_requires=our_requires,
+    package_dir = {'': source_dir},
     packages=our_packages,
-    namespace_packages = ['rabird'],
+    namespace_packages=[namespace],
     )
--- src/rabird/core/distutils.py
+++ src/rabird/core/distutils.py
@@ -5,14 +5,15 @@
 
 '''
 
-
-
 import os
 import os.path
 import shutil
 import sys
 import fnmatch
 import re
+
+PREPROCESSED_DIR = "preprocessed" 
+SOURCE_DIR = "src"
 
 # The shutil.copytree() or distutils.dir_util.copy_tree() will happen to report
 # error list below if we invoke it again and again ( at least in python 2.7.4 ):
@@ -22,66 +23,79 @@ import re
 # So we have to write our's copy_tree() for that purpose.
 #
 def __copy_tree(src_dir, dest_dir):
-	if not os.path.exists(dest_dir):
-		os.makedirs(dest_dir)
-		shutil.copystat(src_dir, dest_dir)
-		
-	for entry in os.listdir(src_dir):
-		from_path = os.path.join(src_dir, entry)
-		to_path = os.path.join(dest_dir, entry)
-		if os.path.isdir(from_path):
-			__copy_tree(from_path, to_path)
-		else:
-			shutil.copy2(from_path, to_path)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+        shutil.copystat(src_dir, dest_dir)
+        
+    for entry in os.listdir(src_dir):
+        from_path = os.path.join(src_dir, entry)
+        to_path = os.path.join(dest_dir, entry)
+        if os.path.isdir(from_path):
+            __copy_tree(from_path, to_path)
+        else:
+            shutil.copy2(from_path, to_path)
 
-##
-# A special method for convert all source files to compatible with current
-# python version during installation time.
-#
-def preprocess_sources_for_compatible(source_path, destination_path):
-	
-	# The 'build' and 'dist' folder sometimes will not update! So we need to 
-	# remove them all !
-	shutil.rmtree(os.path.join(destination_path, 'build'), ignore_errors=True)
-	shutil.rmtree(os.path.join(destination_path, 'dist'), ignore_errors=True)
-	
-	# Remove all unused directories
-	directories = []
-	directory_patterns = ['__pycache__', '*.egg-info']
-	for root, dirs, files in os.walk(destination_path):
-		for adir in dirs:
-			for pattern in directory_patterns:
-				if fnmatch.fnmatch(adir, pattern):
-					directories.append(os.path.join(root, adir))
-					break
-					
-	for adir in directories:
-		shutil.rmtree(adir, ignore_errors=True)
-	
-	# Removed old preprocessed sources.
-	directories = []
-	for item in os.listdir(source_path):
-		path = os.path.join(source_path, item)
-		if os.path.isdir(path):
-			directories.append(item)
-			destination_item_path = os.path.join(destination_path, item)
-			shutil.rmtree(destination_item_path, ignore_errors=True)
-			__copy_tree(path, destination_item_path)
-			
-	if sys.version_info[0] >= 3:
-		# We wrote program implicated by version 3, if python version large or equal than 3,
-		# we need not change the sources.
-		return
-	
-	# Check and prepare 3to2 module.
-	try:
-		from lib3to2.main import main as lib3to2_main
-	except ImportError:
-		import pip
-		
-		pip.main(['install', '3to2'])
-		
-		from lib3to2.main import main as lib3to2_main
-	
-	for folder_name in directories:
-		lib3to2_main("lib3to2.fixes", [os.path.join(destination_path, folder_name)])
+def preprocess_source(base_dir=os.curdir):
+    """
+    A special method for convert all source files to compatible with current
+    python version during installation time.
+    
+    The source directory layout must like this :
+    
+    base_dir --+
+               |
+               +-- src (All sources are placed into this directory)
+               |
+               +-- preprocessed (Preprocessed sources are placed into this
+               |   directory)
+               |
+               +-- setup.py
+               |
+               ...
+               
+    @return Preprocessed source directory 
+    """
+    
+    source_path = os.path.join(base_dir, SOURCE_DIR)
+    destination_path = os.path.join(base_dir, PREPROCESSED_DIR)
+    
+    # The 'build' and 'dist' folder sometimes will not update! So we need to 
+    # remove them all !
+    shutil.rmtree(os.path.join(base_dir, 'build'), ignore_errors=True)
+    shutil.rmtree(os.path.join(base_dir, 'dist'), ignore_errors=True)
+    
+    # Remove all unused directories
+    directories = []
+    directory_patterns = ['__pycache__', '*.egg-info']
+    for root, dirs, files in os.walk(destination_path):
+        for adir in dirs:
+            for pattern in directory_patterns:
+                if fnmatch.fnmatch(adir, pattern):
+                    directories.append(os.path.join(root, adir))
+                    break
+                    
+    for adir in directories:
+        shutil.rmtree(adir, ignore_errors=True)
+    
+    # Removed old preprocessed sources.
+    shutil.rmtree(destination_path, ignore_errors=True)
+    __copy_tree(source_path, destination_path)
+            
+    if sys.version_info[0] >= 3:
+        # We wrote program implicated by version 3, if python version 
+        # large or equal than 3, we need not change the sources.
+        return source_path
+    
+    # Check and prepare 3to2 module.
+    try:
+        from lib3to2.main import main as lib3to2_main
+    except ImportError:
+        import pip
+        
+        pip.main(['install', '3to2'])
+        
+        from lib3to2.main import main as lib3to2_main
+    
+    lib3to2_main("lib3to2.fixes", [destination_path])
+    return destination_path
+
