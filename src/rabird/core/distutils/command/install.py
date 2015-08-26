bd12dcbb4c89f44484f1cd15a01c812464c3c774
Hong-She Liang <starofrainnight@gmail.com>
2015-08-26 16:55:43 +0800

    Replaced GithubUwbpepPackages by PypiUwbpepPackages

--- src/rabird/core/distutils/command/install.py
+++ src/rabird/core/distutils/command/install.py
@@ -148,7 +148,108 @@ class GithubUwbpepPackages(object):
         
         raise KeyError("Can't find the requirement : %s" % requirement_text)
             
+class PypiUwbpepPackages(object):    
+    page_url = "https://pypi.python.org/pypi/uwbpep/0.1.0"
+    
+    def __init__(self):
+        pass
+        
+    def parse(self):
+        print('Downloading list page of "Unofficial Windows Binaries for Python Extension Packages" ...')
+        bytes_io = io.BytesIO()            
+        try:
+            download_file(self.page_url, bytes_io)
+            content = bytes_io.getvalue().decode('utf-8')  
+        finally:
+            bytes_io.close()
+            
+        print("Download finished. \nParsing ...")
+            
+        re_flags = re.DOTALL|re.MULTILINE
+        matched = re.findall('<a href="([^"]*?\.whl#md5=[^"]*?)"', content, re_flags)
+        
+        # Initialize packages with names
+        packages = {}
 
+        # Decrypt links
+        for amatch in matched:
+            url = amatch
+            filename = os.path.basename(url.split("#")[0])
+            # Removed the first "uwbpep" tag!
+            filename = filename[len("uwbpep"):]
+            print(filename)
+            
+            filebasename, fileextname = os.path.splitext(filename)
+            if fileextname not in [".whl", ".exe", ".zip"]:
+                continue
+            
+            if len(filename.split("-")) < 5:
+                continue
+            
+            if fileextname == ".exe":
+                # Fixed *.exe name to fit for Wheel() requirement! A slight trick
+                # to support *.exe package. 
+                
+                filebasename = filebasename.replace('.win-amd64', '-win_amd64')
+                filebasename = filebasename.replace('.win', '-win')
+                filebasename = filebasename.replace('-py2.', '-cp2')
+                filebasename = filebasename.replace('-py3.', '-cp3')
+                
+                wheel_info = filebasename.split('-')
+                filename = "%s-%s-%s-%s-%s.whl" % (
+                    wheel_info[0],
+                    wheel_info[1],
+                    wheel_info[3],
+                    'none',
+                    wheel_info[2]
+                    )
+            elif fileextname == ".zip":
+                filename = "%s%s" % (filebasename, ".whl")
+                
+            wheel = Wheel(filename)
+            
+            package_name = wheel.name.lower().replace("_", "-")
+            if package_name not in packages:
+                packages[package_name] = {}
+                packages[package_name]["wheels"] = []
+                packages[package_name]["requirements"] = []  
+            
+            packages[package_name]["wheels"].append((wheel, url))
+            
+        self.packages = packages        
+        
+    def search_url(self, requirement_text):
+        requirement = pkg_resources.Requirement.parse(requirement_text)
+        wheel_contexts = self.packages[requirement.key]["wheels"]
+        
+        if is_64bit():
+            python_platform = "64"
+        else:
+            python_platform = "32"
+            
+        python_versions = set([
+            "cp%s" % platform.python_version_tuple()[0],
+            "cp%s%s" % (platform.python_version_tuple()[0], platform.python_version_tuple()[1]),
+            "py%s" % platform.python_version_tuple()[0],
+            "py%s%s" % (platform.python_version_tuple()[0], platform.python_version_tuple()[1]),
+            ])        
+                
+        for wheel, url in wheel_contexts:
+            if python_platform not in wheel.plats[0]:
+                continue
+            
+            if len(set(wheel.pyversions) & python_versions) <= 0:
+                continue
+            
+            wheel_version = Version(wheel.version)
+            if not requirement.specifier.contains(wheel_version):
+                continue        
+            
+            return url
+        
+        raise KeyError("Can't find the requirement : %s" % requirement_text)
+       
+       
 class install(distutils_install):
     """
     The install command provide extension packages automatic install from
@@ -177,7 +278,7 @@ class install(distutils_install):
                 failed_requires.append(arequire)
             
         if len(failed_requires) > 0:
-            packages = GithubUwbpepPackages()
+            packages = PypiUwbpepPackages()
             packages.parse()            
               
             # Try to install failed requires from UWBPEP    
