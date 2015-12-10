f83f69edb27ab064327f329ab340febb5c63a2e1
Hong-She Liang <starofrainnight@gmail.com>
2015-12-10 16:38:38 +0800

    Fixed compatible with python 2

--- src/rabird/core/cstring.py
+++ src/rabird/core/cstring.py
@@ -6,9 +6,14 @@ Provided C-style string process methods.
 @author Hong-She Liang <starofrainnight@gmail.com>
 '''
 
+import six
+
 def escape(text):
-    return text.encode('unicode-escape').replace('"', '\\"').replace("'", "\\'")
+    if six.PY2:
+        return text.encode('unicode-escape').replace('"', '\\"').replace("'", "\\'")
+    else:
+        return text.encode('unicode-escape').decode().replace('"', '\\"').replace("'", "\\'") 
 
 def unescape(text):
-    return text.decode('unicode-escape')    
+    return six.b(text).decode('unicode-escape')    
         
