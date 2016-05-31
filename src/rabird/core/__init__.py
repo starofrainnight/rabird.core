12465f6160d36742dc166f723874303837eb120b
Hong-She Liang <starofrainnight@gmail.com>
2016-05-31 22:42:05 +0800

    Update to 0.3.6

--- src/rabird/core/__init__.py
+++ src/rabird/core/__init__.py
@@ -1,23 +1,23 @@
 
 import sys
 
-version_info = (0, 3, 5)
+version_info = (0, 3, 6)
 __version__ = '.'.join(map(str, version_info))
 __monkey_patched = False
 
 # Known Issues : Can't work with eventlet, Why?
 def monkey_patch():
 	global __monkey_patched
-	
+
 	if __monkey_patched:
 		return
-		
+
 	if sys.platform == 'win32' :
 		from . import windows_api
 		from . import windows_fix
 
 		if sys.version_info[0] <= 2 :
 			windows_fix.monkey_patch()
-			
+
 		__monkey_patched = True
-		
+
